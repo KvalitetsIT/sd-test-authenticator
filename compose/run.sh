3e80894454f760b390e97637eac2c5dd748ba86d
nils <njo@kvalitetsit.dk>
2020-10-29 16:11:02 +0100

    Set up nginx in front of keycloak, set various headers.

--- compose/run.sh
+++ compose/run.sh
@@ -9,7 +9,7 @@ echo "Waiting for Keycloak to be ready ..."
 
 max_iterations=10
 iterations=0
-code=$(curl -s -o /dev/null -w "%{http_code}" localhost:8080/auth/)
+code=$(curl -s -o /dev/null -w "%{http_code}" localhost:8787/auth/)
 while [ $code -ne 200 ];
 do
     iterations=$iterations+1;
@@ -20,7 +20,7 @@ do
     
     echo "Keycloak not ready yet, sleeping 5s ..."
     sleep 5;
-    code=$(curl -s -o /dev/null -w "%{http_code}" localhost:8080/auth/)
+    code=$(curl -s -o /dev/null -w "%{http_code}" localhost:8787/auth/)
 done;
 
 echo "Keycloak ready ..."
@@ -29,11 +29,11 @@ echo "Creating user for testing ..."
 
 # Get access token for the REST api
 echo "Getting access token ..."
-token=$(curl -s -d client_id=admin-cli -d username=medcom -d password=Test1234 -d grant_type=password http://localhost:8080/auth/realms/master/protocol/openid-connect/token | jq -r .access_token)
+token=$(curl -s -d client_id=admin-cli -d username=medcom -d password=Test1234 -d grant_type=password http://localhost:8787/auth/realms/master/protocol/openid-connect/token | jq -r .access_token)
 echo "Got access token $token, creating test user ..."
 
 echo "Creating test user in form realm ..."
-code=$(curl -s -d "{\"username\": \"test\", \"credentials\": [ { \"type\": \"password\", \"value\": \"Test1234\" } ], \"enabled\": true}" -H "Authorization: bearer $token" -X POST --header "Content-Type: application/json" -w "%{http_code}" http://localhost:8080/auth/admin/realms/form/users);
+code=$(curl -s -d "{\"username\": \"test\", \"credentials\": [ { \"type\": \"password\", \"value\": \"Test1234\" } ], \"enabled\": true}" -H "Authorization: bearer $token" -X POST --header "Content-Type: application/json" -w "%{http_code}" http://localhost:8787/auth/admin/realms/form/users);
 if [ $code -ne 201 ]; then
     echo "Could not create test user, got statuscode $code back. Failing ..."
     exit 1
